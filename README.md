3f17ca30c8fdb33968a43b498119a14493852b7e
joyqi <joyqi@segmentfault.com>
2022-12-09 16:11:25 +0800

    init project

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # koa-mount-pattern
+
+As you may know, there's a famous middleware called [koa-mount](https://github.com/koajs/mount) which is used to mount a middleware to a specific path. But what if you want to mount a middleware to a path pattern? For example, you want to mount a middleware to all paths that start with `/api/(v1|v2|v3)`? This is where `koa-mount-pattern` comes in.
+
+## Installation
+
+```bash
+npm install koa-mount-pattern
+```
+
+## Usage
+
+```js
+const Koa = require('koa');
+const mount = require('koa-mount-pattern');
+
+const app = new Koa();
+app.use(mount('/api/:version', async (ctx, next) => {
+  // do something
+  await next();
+}));
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-// TODO: export your library
+import { Context, Middleware, Next } from 'koa';
+import { pathToRegexp } from 'path-to-regexp';
+
+declare module 'koa' {
+    interface BaseContext {
+        mountPath: string;
+    }
+}
+
+type MatchResult = {
+    matched: boolean;
+    newPath: string;
+    mountPath: string;
+}
+
+// Mount a middleware function to a path
+export default function mountPattern(path: string | RegExp, middleware: Middleware) {
+    const match = matcher(path);
+
+    return async (ctx: Context, next: Next) => {
+        const prev = ctx.path;
+        const { matched, newPath, mountPath } = match(prev);
+
+        if (matched) {
+            ctx.path = newPath;
+            ctx.mountPath = mountPath;
+            await middleware(ctx, async () => {
+                ctx.path = prev;
+                await next();
+                ctx.path = newPath;
+            });
+            ctx.path = prev;
+        } else {
+            await next();
+        }
+    }
+}
+
+// Create a matcher function from a path string or a regular expression
+function matcher(pattern: string | RegExp): (path: string) => MatchResult {
+    const regexp = typeof pattern === 'string' ? pathToRegexp(pattern) : pattern;
+
+    return (path: string) => {
+        const matches = regexp.exec(path);
+        const matched = !!matches;
+        const mountPath = matched ? matches[0] : '';
+        let newPath = matched ? path.substring(mountPath.length) : path;
+
+        // Ensure that the new path starts with a slash
+        if (!newPath.startsWith('/')) {
+            newPath = `/${newPath}`;
+        }
+
+        return { matched, newPath, mountPath };
+    }
+}
